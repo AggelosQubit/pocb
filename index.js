a89b00e6d42aa5da2b9d84896f52777bf8e46b6e
AggelosQubit <aggelosqubit@gmail.com>
2022-07-08 17:46:48 +0200

    ReTest User Commit Verified 4

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const https   = require('https');
 const fs      = require('fs');
 const PORT    = 8000;
 var app       = require('express');
-
 const options = {
     key: fs.readFileSync('./certs/key.pem'),
     cert: fs.readFileSync('./certs/cert.pem')
 };
-
 https.createServer(options, (req, res) => {
   res.writeHead(200);
   res.end('Hello World\n');
