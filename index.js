ba6854f83e8d3d4e11527e981e5cbcf762a9b6d1
Aggelos-Qubit <hb.styx@gmail.com>
2022-07-08 16:27:41 +0200

    Node HTTPS Server functionnal local Certs & Keys

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Test Git Remote push");
+const https   = require('https');
+const fs = require('fs');
+
+var app     = require('express');
+const options = {
+    key: fs.readFileSync('./certs/key.pem'),
+    cert: fs.readFileSync('./certs/cert.pem')
+};
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(8000);
+
+console.log("server running " )
